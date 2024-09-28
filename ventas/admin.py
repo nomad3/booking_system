f4c74e1e11b875f2def3a7e797fb3fa16656f239
simon-aguilera <saguilera1608@gmail.com>
2024-09-28 13:17:49 -0300

    Update admin.py

--- ventas/admin.py
+++ ventas/admin.py
@@ -11,25 +11,20 @@ class PagoInline(admin.TabularInline):
 
 class VentaReservaAdmin(admin.ModelAdmin):
     inlines = [ReservaProductoInline, PagoInline]
-    list_display = ('id', 'cliente', 'fecha_creacion', 'fecha_reserva', 'total', 'pagado', 'saldo_pendiente', 'estado')
-    readonly_fields = ('total', 'pagado', 'saldo_pendiente')  # Makes these fields read-only since they are calculated
+    list_display = ('id', 'cliente', 'fecha_reserva', 'total', 'pagado', 'saldo_pendiente', 'estado')  # Removed 'fecha_creacion'
     search_fields = ['cliente__nombre']
 
 class ProveedorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'contacto', 'email')
-    search_fields = ['nombre']
 
 class CategoriaProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
-    search_fields = ['nombre']
 
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio_base', 'categoria', 'cantidad_disponible', 'es_reservable')
-    search_fields = ['nombre']
 
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'email', 'telefono')
-    search_fields = ['nombre', 'email', 'telefono']
 
 admin.site.register(Proveedor, ProveedorAdmin)
 admin.site.register(CategoriaProducto, CategoriaProductoAdmin)
