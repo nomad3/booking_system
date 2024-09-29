0c3ee97858429bac52dae34ba67c1b55bfc097c1
simon-aguilera <saguilera1608@gmail.com>
2024-09-29 20:35:15 -0300

    Update admin.py

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,52 +1,59 @@
 from django.contrib import admin
 from .models import Proveedor, CategoriaProducto, Producto, VentaReserva, ReservaProducto, Pago, Cliente
+from django import forms
+
+
+class ReservaProductoForm(forms.ModelForm):
+    class Meta:
+        model = ReservaProducto
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and not self.instance.producto.es_reservable:
+            # Hide 'fecha_agendamiento' for non-reservable products
+            self.fields['fecha_agendamiento'].widget = forms.HiddenInput()
+        else:
+            self.fields['fecha_agendamiento'].required = True
+
 
 class ReservaProductoInline(admin.TabularInline):
     model = ReservaProducto
     extra = 1
+    form = ReservaProductoForm
 
-    # Customize the form fields displayed based on whether the product is reservable
     def get_formset(self, request, obj=None, **kwargs):
         formset = super().get_formset(request, obj, **kwargs)
-        # Dynamically modify the form fields for each instance
-        for form in formset.form.base_fields:
-            # Check if producto is present and is reservable
-            formset.form.base_fields['fecha_agendamiento'].required = False
         return formset
 
-    def get_fields(self, request, obj=None):
-        fields = super().get_fields(request, obj)
-        # Don't show 'fecha_agendamiento' for non-reservable products
-        # Here we check within 'ReservaProducto' objects linked to this 'VentaReserva'
-        if obj:
-            reservaprodutos = obj.reservaprodutos.all()
-            for reservaproduto in reservaprodutos:
-                if not reservaproduto.producto.es_reservable:
-                    fields = [f for f in fields if f != 'fecha_agendamiento']
-        return fields
 
 class PagoInline(admin.TabularInline):
     model = Pago
     extra = 1
 
+
 class VentaReservaAdmin(admin.ModelAdmin):
     inlines = [ReservaProductoInline, PagoInline]
-    # Removed 'fecha_creacion' from list_display
     list_display = ('id', 'cliente', 'fecha_reserva', 'total', 'pagado', 'saldo_pendiente', 'estado')
     search_fields = ['cliente__nombre']
     readonly_fields = ['total', 'pagado', 'saldo_pendiente', 'estado']
 
+
 class ProveedorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'contacto', 'email')
+
 
 class CategoriaProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
 
+
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio_base', 'categoria', 'cantidad_disponible', 'es_reservable')
 
+
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'email', 'telefono')
+
 
 admin.site.register(Proveedor, ProveedorAdmin)
 admin.site.register(CategoriaProducto, CategoriaProductoAdmin)
