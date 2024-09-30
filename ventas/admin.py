2cfecca5401bc097a0ae8cedac2428d10d8703e6
simon-aguilera <saguilera1608@gmail.com>
2024-09-29 21:58:00 -0300

    Update admin.py

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,50 +1,46 @@
 from django.contrib import admin
 from .models import Proveedor, CategoriaProducto, Producto, VentaReserva, ReservaProducto, Pago, Cliente
 
-
 class ReservaProductoInline(admin.TabularInline):
     model = ReservaProducto
     extra = 1
 
     def get_formset(self, request, obj=None, **kwargs):
         formset = super().get_formset(request, obj, **kwargs)
-        for form in formset.forms:
-            form.fields['fecha_agendamiento'].required = form.instance.producto.es_reservable if form.instance.producto else False
+        for form in formset().forms:  # Correct the formset call here
+            if form.instance and form.instance.producto and form.instance.producto.es_reservable:
+                form.base_fields['fecha_agendamiento'].required = True
+            else:
+                form.base_fields['fecha_agendamiento'].widget = admin.widgets.AdminTextInputWidget()  # Hide it for non-reservable products
+                form.base_fields['fecha_agendamiento'].required = False
         return formset
-
 
 class PagoInline(admin.TabularInline):
     model = Pago
     extra = 1
 
-
 class VentaReservaAdmin(admin.ModelAdmin):
     inlines = [ReservaProductoInline, PagoInline]
     list_display = ('id', 'cliente', 'fecha_reserva', 'total', 'pagado', 'saldo_pendiente', 'estado')
     search_fields = ['cliente__nombre']
+    readonly_fields = ['total', 'pagado', 'saldo_pendiente']
 
-    def get_fields(self, request, obj=None):
-        fields = super().get_fields(request, obj)
-        if obj and obj.productos.filter(es_reservable=True).exists():
-            fields.append('fecha_reserva')
-        return fields
-
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # In edit mode, make these fields read-only
+            return self.readonly_fields + ['cliente', 'fecha_reserva']
+        return self.readonly_fields
 
 class ProveedorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'contacto', 'email')
 
-
 class CategoriaProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
-
 
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio_base', 'categoria', 'cantidad_disponible', 'es_reservable')
 
-
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'email', 'telefono')
-
 
 admin.site.register(Proveedor, ProveedorAdmin)
 admin.site.register(CategoriaProducto, CategoriaProductoAdmin)
