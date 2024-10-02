a8728bb218fb7b12f34f3856ecec24e62b125b60
simon-aguilera <saguilera1608@gmail.com>
2024-10-02 01:21:27 -0300

    Update urls.py

--- aremko_project/urls.py
+++ aremko_project/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from ventas.views import servicios_vendidos_view  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('ventas.urls')),
-    path('servicios-vendidos/', servicios_vendidos_view, name='servicios_vendidos'),
-
+    path('servicios-vendidos/', servicios_vendidos_view, name='servicios_vendidos'),  # Asegúrate de que esté aquí
+    path('api/', include('ventas.urls')),  # Incluye las rutas de la aplicación ventas
 ]
