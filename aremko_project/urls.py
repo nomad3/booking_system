2249497208913f633e9cda7712782bc14e45f8b3
simon-aguilera <saguilera1608@gmail.com>
2024-10-02 01:17:27 -0300

    Update urls.py

--- aremko_project/urls.py
+++ aremko_project/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ventas.urls')),
+    path('servicios-vendidos/', servicios_vendidos_view, name='servicios_vendidos'),
+
 ]
