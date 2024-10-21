3c51e8b0c56997db593e3e9f6257afbfce52f2aa
simon-aguilera <saguilera1608@gmail.com>
2024-10-21 14:19:36 -0300

    Update urls.py

--- aremko_project/urls.py
+++ aremko_project/urls.py
@@ -8,9 +8,10 @@ from ventas.views import auditoria_movimientos_view, venta_reserva_list, venta_r
 urlpatterns = [
     path('admin/', admin.site.urls),  # Esta línea es clave para registrar el namespace 'admin'
     path('', inicio_sistema_view, name='inicio_sistema'),  # Nueva vista de inicio
-    path('ventas/', venta_reserva_list, name='venta_reserva_list'),
+    path('ventas/', include('ventas.urls')),  # Incluye las urls de la app 'ventas'
     path('servicios-vendidos/', servicios_vendidos_view, name='servicios_vendidos'),
     path('caja-diaria/', caja_diaria_view, name='caja_diaria'),  # Nueva vista de caja diaria
     path('auditoria-movimientos/', auditoria_movimientos_view, name='auditoria_movimientos'),  # Nueva vista de auditoría
-    path('ventas/<int:pk>/', venta_reserva_detail, name='venta_reserva_detail'),
+    path('venta_reservas/', views.venta_reserva_list, name='venta_reserva_list'),
+    path('venta_reservas/<int:pk>/', views.venta_reserva_detail, name='venta_reserva_detail'),
 ]
